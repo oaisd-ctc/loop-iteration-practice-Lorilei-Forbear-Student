e337ce2bf8356bf01bfe184bd2ff942e71438f25
Lorilei-Forbear-Student <207830@student.oaisd.org>
2024-10-31 14:43:06 +0000

    midway (?) through functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+
+    public static void Main(string[]args)
+    {
+        //Write1ThroughN_While(7);
+        //Write1ThroughN_For(3);
+        //WriteNThrough1_While(9);
+        //WriteNThrough1_For(8);
+    }
+
+    public static void Write1ThroughN_While(int n)
+    {
+        int i = 1;
+        while (i <= n)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+
+    public static void Write1ThroughN_For(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static void WriteNThrough1_While(int n)
+    {
+        int i = 1;
+        while (i<=n)
+        {
+            Console.WriteLine(n);
+            n--;
+        }
+    }
+
+    public static void WriteNThrough1_For(int n)
+    {
+        for (int i = 1; i <= n; n--)
+        {
+            Console.WriteLine(n);
+        }
+    }
+
+    public static void WriteEvensThrough100()
+    {
+        
+    }
+}
