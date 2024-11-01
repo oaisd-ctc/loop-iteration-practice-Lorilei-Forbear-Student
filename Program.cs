31bd5ddc7277b853e446a08b0644592c794163f7
Lorilei-Forbear-Student <207830@student.oaisd.org>
2024-11-01 14:17:11 +0000

    finished write numbers through n. half through /-\

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ public class Program
         //Write1ThroughN_For(3);
         //WriteNThrough1_While(9);
         //WriteNThrough1_For(8);
+        //WriteEvensThrough100();
+        //FindSum(10);
+        OutputRightTriangle(0);
     }
 
     public static void Write1ThroughN_While(int n)
@@ -49,6 +52,51 @@ public class Program
 
     public static void WriteEvensThrough100()
     {
+        int i = 1;
+        while(i <= 100)
+        {
+            i++;
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    public static void FindSum(int n)
+    {
         
+    }
+
+    public static void OutputRightTriangle(int height) 
+    {
+        if(height > 0)
+        {
+            for (int row = 1; row <= height; row++) //row starts at 1, while row is less than or equal to the height it goes over another asterisk
+            {
+                for (int col = 1; col<= row; col++) //same as above but the other direction
+                {
+                    Console.Write("*"); //writes another * w/o starting new line
+                }
+
+                Console.WriteLine(); //starts another line after previous row is done
+            }
+        }
+        else if(height == 0)
+        {
+            Console.WriteLine("0");
+        }
+        else
+        {
+            for (int row = 1; row >= height; row--)
+            {
+                for (int col = 1; col >= height; col--)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
